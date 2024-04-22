cdad97f68b864f56fb5ef221b7712ad3586aa614
Karol <karolzymkowski1990@gmail.com>
2024-04-22 21:33:00 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 6, b = 5;
+        boolean logical = 5 >=7 && 5 > 1;
+        System.out.println(logical);
+    }
+}
+
